a481ca6ce85c4eb202d402de428dba2be2dcd02a
acnormun <anaclara.noronha@weni.ai>
2024-10-27 17:25:18 -0300

    chore: add rows selection

--- src/pages/Agrupamento.vue
+++ src/pages/Agrupamento.vue
@@ -3,34 +3,67 @@
     <q-card>
       <q-card-section>
         <div class="text-h6">Análise por Data</div>
-        {{ filteredData }}
+
       </q-card-section>
 
       <q-card-section>
-        <!-- Filtro por id -->
         <q-input
         :model-value="searchId"
         label="Filtrar por ID"
         type="text"
         dense
         @update:model-value="filterId"
-      />
-        <!-- Filtro de Data -->
-        <q-select
-          v-model="selectedPeriod"
-          :options="periodOptions"
-          label="Filtrar por"
-          outlined
-          dense
         />
-        <!-- Filtro por status -->
-        <q-select
-        v-model="selectedPeriod"
-        :options="periodOptions"
-        label="Filtrar por"
-        outlined
-        dense
-      />
+
+        <div class="q-pa-md">
+          <q-table
+            row-key="id"
+            :rows="filteredData"
+            :columns="columns"
+            v-model:pagination="pagination"
+            :rows-per-page-options="[0]"
+            v-model:selected="selected"
+            :visible-columns="visibleColumns"
+            style="height: 400px"
+            flat bordered
+            virtual-scroll
+            selection="multiple"
+            binary-state-sort
+            no-data-label="I didn't find anything for you"
+          >
+          <template v-slot:top="props">
+            <div class="col-2 q-table__title">Treats</div>
+
+            <q-space />
+
+            <div v-if="$q.screen.gt.xs" class="col">
+              <q-toggle v-for="item in columns" :key="item.name" v-model="visibleColumns" :val="item.name" :label="item.label" />
+            </div>
+            <q-select
+              v-else
+              v-model="visibleColumns"
+              multiple
+              borderless
+              dense
+              options-dense
+              :display-value="$q.lang.table.columns"
+              emit-value
+              map-options
+              :options="columns"
+              option-value="name"
+              style="min-width: 150px"
+            />
+
+            <q-btn
+              flat round dense
+              :icon="props.inFullscreen ? 'fullscreen_exit' : 'fullscreen'"
+              @click="props.toggleFullscreen"
+              class="q-ml-md"
+            />
+          </template>
+        </q-table>
+        </div>
+        {{ filteredData }}
       </q-card-section>
 
       <q-card-section>
@@ -43,99 +76,59 @@
 
 
 <script lang="ts" setup>
-import { onMounted, ref, watch } from 'vue';
-import Chart from 'chart.js/auto';
+import { onMounted, ref } from 'vue';
 import { backupService } from '../services/backupService';
 import { Agrupamento } from 'src/types/agrupamento.type';
-import { format, parseISO } from 'date-fns';
-
 const backupData = ref<Agrupamento[]>([]);
 const searchId = ref<string | number | null>('')
-const selectedPeriod = ref<string>('day');
 const filteredData = ref<Agrupamento[]>([])
+const  pagination= ref({ rowsPerPage: 1000 })
+const selected = ref([])
+
+const columns = [
+  {
+    name: 'id',
+    label: '#ID',
+    field: (row: Agrupamento) => row.id
+  },
+  {
+    name: 'group',
+    label: 'Agrupamento',
+    field: (row: Agrupamento) => row.agrupamento,
+    sortable: true
+  },
+  {
+    name: 'status',
+    label: 'Status',
+    field: (row: Agrupamento) => row.status,
+  },
+  {
+    name: 'createdAt',
+    label: 'Criado em',
+    field: (row: Agrupamento) => row.CreateAt,
+  }
+]
+
+const visibleColumns = ref(['id', 'group'])
 
 onMounted(async () => {
   try {
     backupData.value = await backupService.getBackupData();
     filteredData.value = backupData.value
-    updateChart();
   } catch (error) {
     console.error('Erro ao carregar os dados do backup:', error);
   }
 });
 
-const periodOptions = ref([
-  { label: 'Por Dia', value: 'day' },
-  { label: 'Por Mês', value: 'month' },
-  { label: 'Por Ano', value: 'year' },
-]);
-
-// Função para agrupar os dados por período (dia, mês ou ano)
-const groupByPeriod = (data: Agrupamento[], period: string) => {
-  return data.reduce((acc: Record<string, number>, item: Agrupamento) => {
-    let date: string;
-
-    if (period === 'day') {
-      date = format(parseISO(item.CreateAt || ''), 'yyyy-MM-dd');
-    } else if (period === 'month') {
-      date = format(parseISO(item.CreateAt || ''), 'yyyy-MM');
-    } else if (period === 'year') {
-      date = format(parseISO(item.CreateAt || ''), 'yyyy');
-    } else {
-      return acc;
-    }
-
-    acc[date] = (acc[date] || 0) + 1;
-    return acc;
-  }, {});
-};
 
 const filterId = (id: string | number | null) => {
   searchId.value = id
   if(id){
     filteredData.value = backupData.value.filter(item => item.id?.toLocaleLowerCase().includes(id?.toString().toLocaleLowerCase()))
-    return;
+  }else {
+    filteredData.value = backupData.value
   }
-  filteredData.value = backupData.value
+
 }
-
-let currentChart: Chart | null = null;
-
-const createChart = (labels: string[], data: number[]) => {
-  const ctx = (document.querySelector('canvas') as HTMLCanvasElement).getContext('2d');
-  if (ctx) {
-    if (currentChart) {
-      currentChart.destroy();
-    }
-
-    currentChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels,
-        datasets: [
-          {
-            label: 'Número de Registros',
-            backgroundColor: '#42A5F5',
-            data,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-      },
-    });
-  }
-};
-
-const updateChart = () => {
-  const groupedData = groupByPeriod(backupData.value, selectedPeriod.value);
-  createChart(Object.keys(groupedData), Object.values(groupedData));
-};
-
-
-// Observa mudanças no filtro e atualiza o gráfico
-watch(selectedPeriod, updateChart);
-
 
 </script>
